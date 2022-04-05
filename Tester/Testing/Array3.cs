d97639fbcad5a6c1a4154c11f4fa2d2e7185a505
BuildTools <unconfigured@null.spigotmc.org>
2022-04-05 10:33:25 +0200

    testing

---
+++ Tester/Testing/Array3.cs
@@ -0,0 +1,13 @@
+﻿namespace Tester.Testing
+{
+    public class Array3
+    {
+        // not supported
+        public object[] arr;
+
+        public Array3()
+        {
+            arr = new object[] { new RotationPacket(44, 33, 22), 12, "Hello" };
+        }
+    }
+}
